de5691e72a523a072670951953c735791595a301
chsanderson <33057702+chsanderson@users.noreply.github.com>
2017-11-07 11:25:15 +0000

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$("#btnLogin").click(function (event){
+		window.location.href = "login.html";
+	});
+	$("#btnFeedback").click(function (event){
+		window.location.href = "Feedback.html";
+	});
+	$("#btnFeeds").click(function (event){
+		window.location.href = "homepage.html";
+	});
+	$("#btnLogout").click(function (event){
+		window.location.href = "logout.html";
+	});
+	$("#btnAddPost").click(function (event){
+		window.location.href = "Add Post.html";
+	});
+	$("#btnPost").click(function (event){
+		window.location.href = "post.html";
+	});
+	
+	// $("#btnSendFeedback").click(function (event){
+		
+	// document.alert("You're feedback has been sent");
+	
+	// });
+	
+});
