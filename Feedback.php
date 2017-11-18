0b6f456023604a6025474705cdb8b99f57704a86
chsanderson <33057702+chsanderson@users.noreply.github.com>
2017-11-18 14:00:01 +0000

    Add files via upload

--- Feedback.php
+++ Feedback.php
@@ -9,24 +9,37 @@
 <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
 <script src="redirect.js"></script>
 
-<!-- this means that the feedback can be sent to the group -->
-
-
+<script type="text/javascript" src="https://cdn.emailjs.com/dist/email.min.js"></script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("user_cdqFPvdGoNYhKd0eI182f");
+   })();
+   $(document).ready(function(event){
+   
+   $("#btnSendFeedback").click(function (event){
+		var myform = $("#feedbackSubject").val();
+		console.log(myform);
+	  emailjs.send("gameshub2017email_gmail_com","template_send_form_1", {message : myform});
+	});
+   });
+   
+</script>
+	
 </head>
 <body>
 <div data-role="page" id="home">
-<div data-role="header" role="banner">
+<div data-role="header" id="banner">
 <p>FeedBack</p>
 </div>
 
-<div data-role="main">
-<form id="feedbackSend">
-<textarea rows="27" cols="80">
-
+<div data-role="content" id="content">
+<!-- <form id="comments" method="POST" action="https://api.emailjs.com/api/v1.0/email/send"> -->
+<textarea rows="27" cols="135" id="feedbackSubject">
 </textarea>
-</form>
 <br/>
 <button id="btnSendFeedback">Send Feedback</button>
+<br/>
+<!-- </form> -->
 <br/>
 </div>
 <div data-role="footer">
